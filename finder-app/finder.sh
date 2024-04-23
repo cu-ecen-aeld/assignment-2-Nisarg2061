ff298946f5fb05b5e5a60d5fc9026f8b2b79fd17
Nisarg Khodke <khodkenisarg@gmail.com>
2024-04-24 03:54:13 +0530

    added assignment

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-# Check if the correct number of arguments are provided
 if [ "$#" -ne 2 ]; then
 	echo "Error: Incorrect number of arguments. Usage: $0 <filesdir> <searchstr>"
 	exit 1
 fi
 
-# Assign the arguments to variables
 filesdir=$1
 searchstr=$2
 
-# Check if filesdir and searchstr are specified
 if [ -z "$filesdir" ] || [ -z "$searchstr" ]; then
 	echo "Error: filesdir or searchstr not specified."
 	exit 1
 fi
 
-# Check if filesdir exists and is a directory
 if [ ! -d "$filesdir" ]; then
 	echo "Error: $filesdir is not a directory."
 	exit 1
 fi
 
-# Find the number of files in filesdir and all subdirectories
 num_files=$(find "$filesdir" -type f | wc -l)
 
-# Find the number of matching lines
 num_matching_lines=$(grep -r "$searchstr" "$filesdir" | wc -l)
 
-# Print the number of files and matching lines
 echo "The number of files are $num_files and the number of matching lines are $num_matching_lines"
 
-# Exit with success status
 exit 0
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-# Check if the correct number of arguments are provided
 if [ "$#" -ne 2 ]; then
 	echo "Error: Incorrect number of arguments. Usage: $0 <writefile> <writestr>"
 	exit 1
 fi
 
-# Assign the arguments to variables
 writefile=$1
 writestr=$2
 
-# Check if writefile and writestr are specified
 if [ -z "$writefile" ] || [ -z "$writestr" ]; then
 	echo "Error: writefile or writestr not specified."
 	exit 1
 fi
 
-# Create the directory path if it doesn't exist
 mkdir -p "$(dirname "$writefile")"
 
-# Write the content to the file
 echo "$writestr" >"$writefile"
 
-# Check if the file was created successfully
 if [ $? -ne 0 ]; then
 	echo "Error: Failed to create file $writefile"
 	exit 1
 fi
 
-# Exit with success status
 exit 0
